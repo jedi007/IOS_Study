9c47fedf4b42b810f0d77dbd4cf87b0f4c42aa38
jedi007 <360751194@qq.com>
2019-08-25 10:58:08 +0800

    增加手指触控绘图的demo

---
+++ Swift/Touchtracker/Touchtracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  Touchtracker
+//
+//  Created by 司开发 on 2019/8/24.
+//  Copyright © 2019 李杰. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
---
+++ Swift/Touchtracker/Touchtracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Touchtracker
+//
+//  Created by 司开发 on 2019/8/24.
+//  Copyright © 2019 李杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
