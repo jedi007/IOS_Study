ba39faed53fd21088ce13d39b4a67a1f99e418cc
jedi007 <360751194@qq.com>
2019-09-07 11:15:32 +0800

    使用collectionView代替imageView

---
+++ Swift/Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by 司开发 on 2019/9/7.
+//  Copyright © 2019 李杰. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    //MARK: - UICollectionViewDataSource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+        return cell
+    }
+}
--- Swift/Photorama/Photorama/PhotosViewController.swift
+++ Swift/Photorama/Photorama/PhotosViewController.swift
@@ -10,45 +10,46 @@ import UIKit
 
 class PhotosViewController: UIViewController {
     
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var collectionView: UICollectionView!
     
     var store: PhotoStore!
+    let photoDataSource = PhotoDataSource()
     
     override func viewDidLoad() {
         super.viewDidLoad()
   
-//因无法访问外网服务器。所以自己构建了diyPhoto
+//        //因无法访问外网服务器。所以自己构建了diyPhoto
 //        store.fetchInterestingPhotos {
 //            (photosResult) -> Void in
-//            
+//
 //            switch photosResult {
 //            case let .success(photos):
 //                print("Successfully found \(photos.count)")
-//                
-//                if let firstPhoto = photos.first {
-//                    self.updateImageView(for: firstPhoto)
-//                }
+//
+//                self.photoDataSource.photos = photos
 //            case let .failure(error):
 //                print("Error fetching recent photos: \(error)")
+//                self.photoDataSource.photos.removeAll()
 //            }
+//            self.collectionView.reloadSections(IndexSet(integer: 0))
 //        }
         
         print("==========Date.init(): \(Date.init())")
         let testUrl = NSURL(string: "https://c-ssl.duitang.com/uploads/item/201209/03/20120903183740_c5Tar.jpeg")
         let diyPhoto = Photo(title: "test", photoID: "123", remoteURL: testUrl! as URL, dateTaken: Date.init() )
-        self.updateImageView(for: diyPhoto)
+        
+        var finalPhotos = [Photo]()
+        for i in 1...15 {
+            print(i)
+            finalPhotos.append(diyPhoto)
+        }
+        photoDataSource.photos = finalPhotos
+        
+        collectionView.dataSource = photoDataSource
+        
+        
+        
     }
     
-    func updateImageView(for photo: Photo){
-        store.fetchImage(for: photo){
-            (imageResult) -> Void in
-            
-            switch imageResult{
-            case let .Success(image):
-                self.imageView.image = image
-            case let .Failure(error):
-                print("Error downloading image: \(error)")
-            }
-        }
-    }
+
 }
