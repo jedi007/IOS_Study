bfe8b644b5179f8031ee99d969f4c5f000a729d1
jedi007 <360751194@qq.com>
2019-09-07 13:27:54 +0800

    使用自定义的PhotoCollectionViewCell
    带有等待转子

---
+++ Swift/Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by 司开发 on 2019/9/7.
+//  Copyright © 2019 李杰. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(with image: UIImage?)
+    {
+        if let imageToDisplay = image
+        {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView = nil
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        update(with: nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        update(with: nil)
+    }
+}
